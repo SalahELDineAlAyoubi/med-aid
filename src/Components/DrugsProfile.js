bcbbbf8a48ca07e078e04b44703ccb50698e923e
SalahELDineAlAyoubi <salah.a.al.ayoubi@gmail.com>
2023-02-11 17:42:26 +0200

    update drugs profile

--- src/Components/DrugsProfile.js
+++ src/Components/DrugsProfile.js
@@ -5,6 +5,7 @@ import { fetchMedecines } from '../Redux/Medecines/medecineActions';
 import { connect } from 'react-redux';
 //import axios from 'axios';
  import { UilPen } from "@iconscout/react-unicons";
+import MedecineModal from "./Medecine Model/MedecineModel";
 
 const DrugsProfile = ({ id }) => {
   // const item= medecines.find(item => item.id === id);
@@ -16,29 +17,29 @@ const DrugsProfile = ({ id }) => {
 
   return (
     <>
-          {medecines.slice(0, 3).map((item) => (
-              // <DrugsProfile key={item.id}/>
-              <div className="ProfileDRugs">
-                <div className="profileCover">
-                  <div className="ProfileDRugsCover">
-                    <div className="profileInfo">
-                      <h4 className="profileDrugsInfo">
-                        {item.name}
-                      </h4>
-                      <span className="profileInfoDesc">
-                        {item.dosage}
-                      </span>
-                    </div>
-                  </div>
-
-                  <img
-                    className="profileDrugImg"
-                    src={item.image}
-                  />
+      {medecines.slice(0, 3).map((item) => (
+        // <DrugsProfile key={item.id}/>
+        <div className="ProfileDRugs" key={item.id}>
+          <div className="profileCover">
+            <div className="ProfileDRugsCover">
+              <div className="profileInfo">
+                <div className="penMed">
+                  <UilPen onClick={() => setModalOpened(true)} />
+                  <MedecineModal
+                    modalOpened={modalOpened}
+                    setModalOpened={setModalOpened}
+                  /> 
                 </div>
+                <h4 className="profileDrugsInfo">{item.name}</h4>
+                <span className="profileInfoDesc">{item.dosage}</span>
               </div>
-            ))}
-            </>
+            </div>
+
+            <img className="profileDrugImg" src={item.image} />
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
