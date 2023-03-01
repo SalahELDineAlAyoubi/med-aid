3c4aeb0479061585e21b4833c076fbb931e5e2a6
SalahELDineAlAyoubi <salah.a.al.ayoubi@gmail.com>
2023-03-01 18:27:19 +0200

    add model add med form

--- src/Components/AddMedModal/AddMedModal.js
+++ src/Components/AddMedModal/AddMedModal.js
@@ -2,7 +2,7 @@ import { Modal, useMantineTheme } from "@mantine/core";
 import "./AddMedModal.css";
 function AddMedModal({ modalOpened, setModalOpened }) {
   const theme = useMantineTheme();
- 
+
   return (
     <Modal
       className="model"
@@ -13,75 +13,93 @@ function AddMedModal({ modalOpened, setModalOpened }) {
       }
       overlayOpacity={0.55}
       overlayBlur={0.3}
-      size="55%"
+      size="70%"
       opened={modalOpened}
       onClose={() => setModalOpened(false)}
     >
-      <form className="infoForm">
-         <h3>Your Med Info</h3>
-          <div>
-            <input
-              type="text"
-              className="infoInput"
-              name="nameMed"
-              placeholder="Name... "
-            />
+      <div className="container">
+        <form className=" ">
+          <h3 className="textHeading">Your Med info</h3>
+
+          <div className="row">
+            <div className="col-12">
+              <input
+                type="text"
+                className="medInput"
+                name="nameMed"
+                placeholder="Name... "
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <input
+                type="text"
+                name="dosageMed"
+                placeholder="Dosage..."
+                className="medInput"
+              />
+            </div>
+            <div className="col-12 col-md-6">
+              <input
+                type="text"
+                name="locationMed"
+                placeholder="Location..."
+                className="medInput"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <input
+                type="text"
+                className="medInput"
+                name="quantityMed"
+                placeholder="Quantity..."
+              />
+            </div>
           </div>
 
-          <div>
-            <input
-              type="text"
-              className="infoInput"
-              name="dosageMed"
-              placeholder="Dosage..."
-            />
-            <input
-              type="text"
-              className="infoInput"
-              name="locationMed"
-              placeholder="Location..."
-            />
+          <div className="row date">
+            <div className="col-12 col-md-6">
+              <span> Open Date : </span>
+              <input
+                type="date"
+                id="start"
+                className="medInput"
+                name="trip-start"
+                value="2022-07-22"
+                min="2010-01-01"
+                max="2023-12-31"
+              ></input>
+            </div>
+            <div className="col-12 col-md-6 exp">
+              <span>Expiry Date : </span>
+
+              <input
+                type="date"
+                id="start"
+                className="medInput"
+                name="trip-start"
+                value="2023-01-01"
+                min="2023-01-01"
+                max="2100-12-31"
+              ></input>
+            </div>
           </div>
 
-          <div>
-            <input
-              type="text"
-              className="infoInput"
-              name="quantityMed"
-              placeholder="Quantity..."
-            />
+          <div className="row" style={{ marginTop: "10px" }}>
+            <div className="col-12">
+              <input type="file" name="profileImg" className="medInput" />
+            </div>
           </div>
-          <div className="date">
-            <span className="opExp"> Op. : </span>
-            <input
-              type="date"
-              id="start"
-              name="trip-start"
-              value="2022-07-22"
-              min="2023-01-01"
-              max="2023-12-31"
-            ></input>
-            <span className="opExp">Exp.: </span>
-
-            <input
-              type="date"
-              id="start"
-              name="trip-start"
-              value="2022-07-22"
-              min="2023-01-01"
-              max="2023-12-31"
-            ></input>
+          <div className="row butt" style={{ marginTop: "10px" }}>
+            <div className="row">
+              <button className="button infoButton">Add</button>
+            </div>
           </div>
-          <div className="browse">
-            <span className="opExp" style={{ color: "#4A4EB7" }}>
-              Image
-            </span>
-            <input type="file" name="profileImg" />
-          </div>
-
-          <button className="button infoButton">Update</button>
-   
-      </form>
+        </form>
+      </div>
     </Modal>
   );
 }
