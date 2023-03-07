f2316c82204174e2b7a5fac9247db89dda9e42b4
SalahELDineAlAyoubi <salah.a.al.ayoubi@gmail.com>
2023-03-07 13:47:18 +0200

    book Med

--- src/Components/CardMed/CardMed.js
+++ src/Components/CardMed/CardMed.js
@@ -7,26 +7,40 @@ import { useEffect } from 'react';
  import { findChat } from '../../Redux1/api/ChatRequest';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '../../Redux1/actions/UserActions';
- 
+import { bookMed } from '../../Redux1/api/PostsRequests';
+  
 const CardMed = ({ item, usero, loading }) => {
   const [modalOpened, setModalOpened] = useState(false);
   const [available, setAvailable] = useState(true);
   const [userData, setUserData] = useState({});
-  //const { users, loading } = useSelector((state) => state.userReducer);
-  const dispatch = useDispatch();
+    const {user} = useSelector((state) => state.authReducer.authData)||{};
+  const [postData, setPostData] = useState(item); 
 
-const toggleAvailability = (id) => {
-  // setAvailable(
-  //   medData.map((item) => {
-  //     if (item.id === id) {
-  //       return { ...item, available: !item.available };
-  //     }
-  //     return item;
-  //   })
-  // );
-  setAvailable(false);
+   const dispatch = useDispatch();
+ useEffect(() => {
+   if (item.taken == 1) setAvailable(false);
+ }, [item]); 
+  const toggleAvailability = (id) => {
+    // setAvailable(
+    //   medData.map((item) => {
+    //     if (item.id === id) {
+    //       return { ...item, available: !item.available };
+    //     }
+    //     return item;
+    //   })
+    // );
+ ;
 
-};
+    if (item.taken == 0) {
+      if(user){
+        console.log(postData._id);
+        console.log(user._id);
+        console.log(item);
+        // bookMed(postData._id, user._id);
+        setAvailable(false);
+      }
+    } 
+  };
   /* useEffect(() => {
     const getUserData = async () => {
       try {
@@ -39,12 +53,12 @@ const toggleAvailability = (id) => {
     if (item !== null) getUserData();
   }, [item]);*/
   useEffect(() => {}, []);
- 
+
   return (
     <div className="medList">
       <div className="medCard">
         <div className="medUsername"> {usero.name}</div>
-        <div className="date"> {format(item.updatedAt)}</div>
+        <div className="date"> {format(item.createdAt)}</div>
         <div className="img">
           <img
             src={
@@ -87,16 +101,20 @@ const toggleAvailability = (id) => {
           />
         </div>
         {available ? (
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAACdklEQVR4nO2WyWsUQRSHv2gQV4y4kXRVtYlgQDToyYOQiFGzqHHBi4fkIAoes+JMLdZBQUGjxslf4T8ZebPE2aeTzDAQfPCgu6q73lfvV6+q4L912yKDieOecqwrx4+yr6vAlPTRS0sjQ9qT157tpu7IyTe9iR4ZbBu8CmLfmbgeOaYck8rxseyT0iZ9kvZWQRPLnHK8rryrPFN7Cnwxcjr1zBvH1/rBpc3kOKc8Gy1mvDW+wRnjibsAnrVMgYcjF4znlSykdmlVK5xQns2mfZY3I57xmjbPZtvAxjFqPG+1o9BRU892GjleDWAci6nntvZsSXCBqMvK98aoOwwkkQntWM0StEbjiKqRwLGV5JkYyXFN0i/vdRlolCCNDMlCUZ6fewYIzEmd12ufWm4lnvkGWZotQm1ZVnlujH3grPYsaMu3rACyEM+XZprLVIZ/ONoI4Hc/ckngznDkpHHc155PGSHeSTW0hWi3Een6NDk+mzzTl9c4JUA6YFsOHLAVaClZSbHoLAuz7GvFtDebecVaDl6SYkGkEYlEqqq+BtmU54tyzCpLIptUcfOyJHQy3TnFv2SRjkUuFcvUMSrP0iZ9nf4/MID+p2PBBO6KZ90nugvgS2VnLI/38k93ASxzJvQRQDlmjedJ/wACM9rytH8Alhkd+gigHY+KdX/YAFYyDxh4qC3PMn9vWe4IwA4DynOz5dWqyo3jgfI8z5CpnJwRRI50Bqgy7biqA+/3DeBYlckAAxzEkojSniXt+V0DEJhWlhd1QQsCfcWR0m0brlxOy7clOaZVKAGUD6IlOZjotaWRocTzUg4i2YxkO5bzv+eBOcz2F+oqf+fil22mAAAAAElFTkSuQmCC"
-                    className="availableIcon"
-                    alt="icon"
-                  />
-                ) : (
-                  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABvUlEQVR4nO2WUUsbQRSFP41FY0sVLBWfmj4JEtE2Wq0U+9CHlr6kRlbpg6Big/n/P+DIkLNlWDebTXYXofXCgdmZ2Tln5s69d+DZajZBS7ArGAjujIH7Wk2TvxRcCEYTkIQ5Te78whjkkA88lsx9EoIlQVfw2wjtJY/tRkRtQT8i77vv3N/dWYnbgkPBTc7OQt+raPGYsJ9p/z2NssRrglPBnwK/Biz7ssV9KXGWPOBuGvGm4LvgfgpxiheVBQgWBO8FZyVJY7yp7AKNQ+irYDiHgIPKl1DjSe8Eq4Ijwe0MAq5NkDjU4tNIEfouPWcxT8DICJO2fbH2BFclRfxwNCRzJSI9nnxlASsWdFmwcCy67fyQTcXd3J2nVrD4rV2yahdlfZt1W3BHT7DhxBWwwTTT9CMe+pKuO0w33S51cesQMDJCbtgxyuaJWgWMHHaHs/xTt4Ce4NOzAD2hCz467P5fAR8Ex3UL+NWggH4ZAQuCzoRKNq+AxDVicg3IM8GW4GfBwvuCzwXj4VHToapp/NL5lpNy8wTcW/TbysRZE7wWfImKTijTJ24PLXKdpk3jB8eJC1GajtuNE/Mv2wMh9hQJBFvbKgAAAABJRU5ErkJggg=="
-                    className="availableIcon"
-                    alt="icon"
-                  />
-                )}
+          <img
+            style={{ cursor: "pointer" }}
+            onClick={toggleAvailability}
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAACdklEQVR4nO2WyWsUQRSHv2gQV4y4kXRVtYlgQDToyYOQiFGzqHHBi4fkIAoes+JMLdZBQUGjxslf4T8ZebPE2aeTzDAQfPCgu6q73lfvV6+q4L912yKDieOecqwrx4+yr6vAlPTRS0sjQ9qT157tpu7IyTe9iR4ZbBu8CmLfmbgeOaYck8rxseyT0iZ9kvZWQRPLnHK8rryrPFN7Cnwxcjr1zBvH1/rBpc3kOKc8Gy1mvDW+wRnjibsAnrVMgYcjF4znlSykdmlVK5xQns2mfZY3I57xmjbPZtvAxjFqPG+1o9BRU892GjleDWAci6nntvZsSXCBqMvK98aoOwwkkQntWM0StEbjiKqRwLGV5JkYyXFN0i/vdRlolCCNDMlCUZ6fewYIzEmd12ufWm4lnvkGWZotQm1ZVnlujH3grPYsaMu3rACyEM+XZprLVIZ/ONoI4Hc/ckngznDkpHHc155PGSHeSTW0hWi3Een6NDk+mzzTl9c4JUA6YFsOHLAVaClZSbHoLAuz7GvFtDebecVaDl6SYkGkEYlEqqq+BtmU54tyzCpLIptUcfOyJHQy3TnFv2SRjkUuFcvUMSrP0iZ9nf4/MID+p2PBBO6KZ90nugvgS2VnLI/38k93ASxzJvQRQDlmjedJ/wACM9rytH8Alhkd+gigHY+KdX/YAFYyDxh4qC3PMn9vWe4IwA4DynOz5dWqyo3jgfI8z5CpnJwRRI50Bqgy7biqA+/3DeBYlckAAxzEkojSniXt+V0DEJhWlhd1QQsCfcWR0m0brlxOy7clOaZVKAGUD6IlOZjotaWRocTzUg4i2YxkO5bzv+eBOcz2F+oqf+fil22mAAAAAElFTkSuQmCC"
+            className="availableIcon"
+            alt="icon"
+          />
+        ) : (
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABvUlEQVR4nO2WUUsbQRSFP41FY0sVLBWfmj4JEtE2Wq0U+9CHlr6kRlbpg6Big/n/P+DIkLNlWDebTXYXofXCgdmZ2Tln5s69d+DZajZBS7ArGAjujIH7Wk2TvxRcCEYTkIQ5Te78whjkkA88lsx9EoIlQVfw2wjtJY/tRkRtQT8i77vv3N/dWYnbgkPBTc7OQt+raPGYsJ9p/z2NssRrglPBnwK/Biz7ssV9KXGWPOBuGvGm4LvgfgpxiheVBQgWBO8FZyVJY7yp7AKNQ+irYDiHgIPKl1DjSe8Eq4Ijwe0MAq5NkDjU4tNIEfouPWcxT8DICJO2fbH2BFclRfxwNCRzJSI9nnxlASsWdFmwcCy67fyQTcXd3J2nVrD4rV2yahdlfZt1W3BHT7DhxBWwwTTT9CMe+pKuO0w33S51cesQMDJCbtgxyuaJWgWMHHaHs/xTt4Ce4NOzAD2hCz467P5fAR8Ex3UL+NWggH4ZAQuCzoRKNq+AxDVicg3IM8GW4GfBwvuCzwXj4VHToapp/NL5lpNy8wTcW/TbysRZE7wWfImKTijTJ24PLXKdpk3jB8eJC1GajtuNE/Mv2wMh9hQJBFvbKgAAAABJRU5ErkJggg=="
+            className="availableIcon"
+            alt="icon"
+          />
+        )}
 
         <div className="medCard__content">
           <h1 className="medName">{item.name}</h1>
--- src/Redux1/api/PostsRequests.js
+++ src/Redux1/api/PostsRequests.js
@@ -6,4 +6,5 @@ const API = axios.create({ baseURL: "http://localhost:4000" });
 export const getPosts = () => API.get(`/posts/`);
 export const updatePost = (id, formData) => API.put(`/posts/${id}`, formData);
 export const deletePost = (id, userId) => API.delete(`/posts/${id}`,{data:{userId}});
+export const bookMed = (id, userId) =>API.put(`/posts/book/${id}`,  userId );
 
